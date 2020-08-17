24ef17a07019d6c54b456177d870ce964feb1704
dieggo111 <marius.metzler@kit.edu>
2020-08-17 22:52:20 +0200

    added main docker-compose for setting up reverse proxy

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM php:7.4-cli
 LABEL maintainer="marius.metzler@gmx.net"
 COPY . /usr/src/
 WORKDIR /usr/src/
+EXPOSE 8080
+CMD [ "php", "-S", "0.0.0.0:8080" ]
 #CMD [ "php", "./your-script.php" ]
--- app/src/utils.js
+++ app/src/utils.js
@@ -1,4 +1,5 @@
-var backendAddress = 'http://localhost:8000/'
+var backendAddress = 'http://backend:8080/'
+// var backendAddress = 'http://localhost:8000/'
 
 function convertSqlArrays(array) {
     for (let i=0; i<array.length; i++) {
